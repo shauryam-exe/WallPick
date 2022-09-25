5816f5fee65bdb1a3fe1ba8a2d20ff5d2bb87674
Shauryam Saxena <saxena.shauryam@gmail.com>
2022-09-25 23:42:39 +0530

    nav drawer working

--- app/src/main/java/com/code/wallpick/ui/home/HomeActivity.kt
+++ app/src/main/java/com/code/wallpick/ui/home/HomeActivity.kt
@@ -2,11 +2,67 @@ package com.code.wallpick.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.code.wallpick.R
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Test Action Bar"
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        navigationView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_account -> Toast.makeText(
+                    this@HomeActivity,
+                    "My Account Clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+                R.id.nav_logout -> Toast.makeText(
+                    this@HomeActivity,
+                    "Logout Clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            Log.d("shauryam", "$item item selected")
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
